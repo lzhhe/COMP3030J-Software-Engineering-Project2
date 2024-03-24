1c4ebd5fb6c9cfcfad255fd96d87b92e8a6e9093
XuZhong <1574030430@qq.com>
2024-03-24 16:20:49 +0800

    修改基础的配置信息

--- app.py
+++ app.py
@@ -1,12 +1,61 @@
-from flask import Flask
+from flask import Flask, session, g, request, jsonify, redirect, url_for, send_from_directory, make_response
+from flask_babel import Babel, gettext as _
+from App import create_app
+from App.extents import db
 
-app = Flask(__name__)
+app = create_app()
+
+app.config['BABEL_DEFAULT_LOCALE'] = 'en'
+app.config['BABEL_DEFAULT_TIMEZONE'] = 'UTC'
+
+babel = Babel(app)
 
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+@babel.localeselector
+def get_locale():
+    return request.accept_languages.best_match(['zh', 'en'])
+
+
+# 这一块是为了实现用户登录后，自动获取用户信息，并将其存储在全局变量g中，供模板文件使用
+# 并且是确保用户登录后才能访问某些界面，否则会重定向到登录页面
+# @app.before_request
+# def my_before():
+#     uid = session.get('uid')
+#     if uid:
+#         user = db.session.get(User, uid)
+#         setattr(g, 'user', user)
+#     else:
+#         setattr(g, 'user', None)
+
+# 全局user变量
+@app.context_processor
+def my_context():
+    return {'user': g.user}
+
+
+# 根据不同的用户状态，返回不同的上级页面
+@app.route('/back')
+def back():
+    # 检查用户是否登录
+    if g.user:
+        if g.user.status == 0:
+            return redirect(url_for('cal_a.adminView'))
+        if g.user.status == 1:
+            # 从会话中获取上一个子页面的标识
+            last_page = session.get('last_page', 'yearView')
+
+            # 根据上一个子页面的标识来决定重定向到哪个页面
+            if last_page == 'weekView':
+                return redirect(url_for('cal_u.weekView'))
+            elif last_page == 'monthView':
+                return redirect(url_for('cal_u.monthView'))
+            elif last_page == 'yearView':
+                return redirect(url_for('cal_u.yearView'))
+        if g.user.status == 2:
+            return redirect(url_for('cal_t.teacherView'))
+    else:
+        return redirect(url_for('cal_u.main'))
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', port=5000, debug=True)
