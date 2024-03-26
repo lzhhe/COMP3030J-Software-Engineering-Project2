3da961e0750747cc51dea1a84c9ffcf75b681f85
nmh21207525 <949400110@qq.com>
2024-03-26 18:38:06 +0800

    update switch

--- App/static/js/switch.js
+++ App/static/js/switch.js
@@ -2,9 +2,19 @@ $(document).ready(function () {
 // 一开始就检查存储的主题和语言设置
     const theme = $('#themeSwitch input');
     const language = $('#languageSwitch input');
-// 监听主题切换的点击事件
+    const html = $('html');
+    if (localStorage.getItem('theme') === 'dark') {
+        html.attr('data-theme', 'dark');
+        theme.prop('checked', true);// 监听主题切换的点击事件
+    }
+    if (localStorage.getItem('language') === 'zh') {
+        html.attr('lang', 'en');
+        language.prop('checked', true);
+    }
+
+
+    html.attr('data-theme', 'dark');
     theme.change(function () {
-        const html = $('html');
         if (html.attr('data-theme') === 'dark') {
             html.attr('data-theme', 'light');
             localStorage.setItem('theme', 'light');
@@ -16,8 +26,7 @@ $(document).ready(function () {
 
 // 监听语言切换的点击事件
     language.change(function () {
-        const html = $('html');
-        if (savedLanguage === 'en') {
+        if (html.attr('lang') === 'en') {
             html.attr('lang', 'zh');
             localStorage.setItem('language', 'zh');
         } else {
