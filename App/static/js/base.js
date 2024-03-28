7ea336916e21e565501a195bf3e6bf90d0ec77cf
ziqigit <ziqi.wang2@ucdconnect.ie>
2024-03-28 20:18:12 +0800

    little base html

---
+++ App/static/js/base.js
@@ -0,0 +1,6 @@
+const body = document.querySelector("body")
+    modeToggle = body.querySelector(".mode-toggle")
+
+modeToggle.addEventListener("click", () =>{
+    body.classList.toggle("dark")
+})
