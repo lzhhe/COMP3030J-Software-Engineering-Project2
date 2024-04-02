0c55ca544da07b109217650d9a75ad8d623d3e02
lzhhe <67408547+lzhhe@users.noreply.github.com>
2024-04-02 14:44:27 +0800

    database update

--- App/views_department.py
+++ App/views_department.py
@@ -15,37 +15,37 @@ def index():
     return render_template('department/index.html')
 
 
-# @department.route('/register', methods=['POST'])
-# def register():
-#     if not request.is_json:
-#         return jsonify({"error": "Missing JSON in request"}), 400
-#
-#     data = request.get_json()
-#
-#     department_id = data.get('departmentID')
-#     order_name = data.get('orderName')
-#     weight = data.get('weight')
-#     attribution = data.get('attribution')
-#     comment = data.get('comment', '')  # 如果没有提供，使用空字符串
-#     # order_status = data.get('orderStatus', 'UNCONFIRMED')
-#
-#     # 验证数据是否完整
-#     if not all([department_id, order_name, weight, attribution]):
-#         return jsonify({"error": "Missing required data for the order"}), 400
-#
-#     # 创建新的工单记录
-#     new_order = Order(
-#         DID=department_id,
-#         date=datetime.utcnow(),
-#         orderName=order_name,
-#         weight=weight,
-#         attribution=attribution,
-#         comment=comment
-#     )
-#
-#     # 添加到数据库并提交
-#     db.session.add(new_order)
-#     db.session.commit()
-#
-#     # 返回成功消息
-#     return jsonify({"message": "Order created successfully"}), 201
+@department.route('/register', methods=['POST'])
+def register():
+    if not request.is_json:
+        return jsonify({"error": "Missing JSON in request"}), 400
+
+    data = request.get_json()
+
+    department_id = data.get('departmentID')
+    order_name = data.get('orderName')
+    weight = data.get('weight')
+    attribution = data.get('attribution')
+    comment = data.get('comment', '')  # 如果没有提供，使用空字符串
+    # order_status = data.get('orderStatus', 'UNCONFIRMED')
+
+    # 验证数据是否完整
+    if not all([department_id, order_name, weight, attribution]):
+        return jsonify({"error": "Missing required data for the order"}), 400
+
+    # 创建新的工单记录
+    new_order = Order(
+        DID=department_id,
+        date=datetime.utcnow(),
+        orderName=order_name,
+        weight=weight,
+        attribution=attribution,
+        comment=comment
+    )
+
+    # 添加到数据库并提交
+    db.session.add(new_order)
+    db.session.commit()
+
+    # 返回成功消息
+    return jsonify({"message": "Order created successfully"}), 201
