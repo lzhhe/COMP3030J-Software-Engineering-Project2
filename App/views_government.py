3ea5f417019f4475faf7ede0efbd44127525956f
lzhhe <67408547+lzhhe@users.noreply.github.com>
2024-05-09 21:01:33 +0800

    weight forecast

--- App/views_government.py
+++ App/views_government.py
@@ -3,6 +3,8 @@ from datetime import datetime, timedelta
 from functools import wraps
 from random import random
 
+from collections import defaultdict
+
 import numpy as np
 import pandas as pd
 from flask import Blueprint, render_template, request, redirect, session, url_for, g, app, jsonify
@@ -13,6 +15,8 @@ from werkzeug.security import generate_password_hash, check_password_hash
 
 from .views_utils import *
 
+from .models import *
+
 government = Blueprint('government', __name__, url_prefix='/government')  # government is name of blueprint
 
 
@@ -22,7 +26,7 @@ def index():
 
 
 def buildKmeansDataset():
-    orders = Order.query.filter(Order.orderStatus.name == "FINISHED").all()
+    orders = Order.query.filter(Order.orderStatus == "FINISHED").all()
     dataset = defaultdict(list)
 
     for order in orders:
@@ -31,6 +35,8 @@ def buildKmeansDataset():
         duration = (order.finishDate - order.date).days
 
         dataset[waste_type_name].append([weight, duration])
+
+    print("KmeansDataset: ", dict(dataset))
 
     return dict(dataset)
 
@@ -54,52 +60,150 @@ def TypeKmeansCenter(dataset, k=1):  # 聚类中心为1
     return centers
 
 
-def buildARIMADataset():
-    orders = Order.query.filter(Order.orderStatus.name == "FINISHED").all()
-    dataset = defaultdict(list)
-
-    for order in orders:
-        waste_type_name = order.wasteType.name
-        weight = order.weight
-        orderDate = order.date
-
-        dataset[waste_type_name].append([weight, orderDate])
-
-    return dict(dataset)
-
-
-def forecast_weight(dataset, days=7, order=(1, 1, 1)):  # 时间序列数据的自回归（AR）、差分（I）和移动平均（MA）部分
-    # 考虑前一期的观察值（自回归阶数为1）、一阶差分（以使时间序列平稳）以及前一期的误差项（移动平均阶数为1）来预测当前值。
-    forecasts = {}
-
-    for category, data in dataset.items():
-        # 将数据转换为 DataFrame 格式
-        df = pd.DataFrame(data, columns=['Weight', 'Date'])
-        df['Date'] = pd.to_datetime(df['Date'])
-        df.set_index('Date', inplace=True)
-
-        # 创建 ARIMA 模型并拟合
-        model = ARIMA(df, order=order)
-        fitted_model = model.fit()
-
-        forecast = fitted_model.forecast(steps=days)
-
-        forecasts[category] = forecast
-
-    return forecasts
-
-
 @government.route('/build_kmeans')
 def build_kmeans():
     dataset = buildKmeansDataset()
     centers = TypeKmeansCenter(dataset)
-    # Perform any additional processing or inference here
+    centers = {category: center.tolist() for category, center in centers.items()}
+    print("KMeans Centers:", centers)
     return jsonify({'dataset': dataset, 'centers': centers})
+
+
+def generate_array():
+    current_date = datetime.now().date()
+    date_values = [[current_date - timedelta(days=i), 0.0] for i in range(30)]
+    return date_values
+
+
+def buildWeightDataset():
+    # 获取最近30天内的订单数据
+    thirty_days_ago = datetime.now() - timedelta(days=30)
+    orders = Order.query.filter(Order.orderStatus == "FINISHED", Order.date >= thirty_days_ago).all()
+    dataset = defaultdict(list)
+    dateList = generate_array()  # [date, 0]
+
+    # 构建每个类别的时间序列
+    for order in orders:
+        waste_type_name = order.wasteType.name
+        multiplier = order.multiplier
+        weight = float(order.weight * multiplier)  # 将重量转换为float类型，乘以倍率
+        order_date = order.date
+
+        # 检查废物类型是否在数据集中，如果不在，则初始化为 dateList
+        if waste_type_name not in dataset:
+            dataset[waste_type_name] = [entry.copy() for entry in dateList]  # 深拷贝 dateList
+
+        for entry in dataset[waste_type_name]:
+            if entry[0] == order_date:
+                entry[1] = entry[1] + weight
+                break
+
+    # 对每个类别的数据按日期进行排序
+    for category in dataset:
+        dataset[category].sort(key=lambda x: x[0])
+
+    # print(dict(dataset))
+    return dict(dataset)
+
+
+def forecastWeight(dataset, days=5, order=(1, 1, 1)):
+    forecasts = {}
+    for category, data in dataset.items():
+        # 提取每个类别的重量和日期数据
+        dates = [entry[0] for entry in data]
+        weights = [entry[1] for entry in data]
+        df = pd.DataFrame({'Date': dates, 'Weight': weights})
+
+        df['Date'] = pd.to_datetime(df['Date'])
+        df.set_index('Date', inplace=True)
+        df.index.freq = 'D'
+        # 应用ARIMA模型
+        model = ARIMA(df, order=order, freq='D')
+
+        fitted_model = model.fit()
+
+        # 进行预测
+        forecast = fitted_model.forecast(steps=days)
+        forecast_values = forecast.values.tolist()
+
+        forecasts[category] = forecast_values
+
+    return forecasts
+
+
+def generate_time_array():
+    current_date = datetime.now().date()
+    date_values = [[current_date - timedelta(days=i), []] for i in range(30)]
+    return date_values
+
+
+def buildTimeDataset():
+    # 获取最近30天内的订单数据
+    thirty_days_ago = datetime.now() - timedelta(days=30)
+    orders = Order.query.filter(Order.orderStatus == "FINISHED", Order.date >= thirty_days_ago).all()
+    dataset = defaultdict(list)
+    dateList = generate_time_array()  # 形式为[date, []]
+
+    # 构建每个类别的时间序列
+    for order in orders:
+        waste_type_name = order.wasteType.name
+        order_date = order.date
+        process_time = (order.finishDate - order.date).days  # 计算处理时间（天）
+
+        # 检查废物类型是否在数据集中，如果不在，则初始化为 dateList
+        if waste_type_name not in dataset:
+            dataset[waste_type_name] = [entry.copy() for entry in dateList]  # 深拷贝 dateList
+
+        for entry in dataset[waste_type_name]:
+            print(entry)
+            if entry[0] == order_date:
+                if waste_type_name == "HEAVY_METAL_WASTEWATER":
+                    print(order_date,process_time)
+                entry[1].append(process_time)  # 创建新的处理时间列表并添加处理时间
+                break
+
+        print(dict(dataset))
+
+    print(dict(dataset))
+    return dict(dataset)
+
+
+def forecastTime(dataset, days=5, order=(1, 1, 1)):
+    forecasts = {}
+    for category, data in dataset.items():
+        # 提取每个类别的重量和日期数据
+        dates = [entry[0] for entry in data]
+        print(dates)
+        times = [entry[1] for entry in data]
+        print(times)
+        df = pd.DataFrame({'Date': dates, 'Times': times})
+
+        df['Date'] = pd.to_datetime(df['Date'])
+        df.set_index('Date', inplace=True)
+        df.index.freq = 'D'
+        # 应用ARIMA模型
+        model = ARIMA(df, order=order, freq='D')
+
+        fitted_model = model.fit()
+
+        # 进行预测
+        forecast = fitted_model.forecast(steps=days)
+        forecast_values = forecast.values.tolist()
+
+        forecasts[category] = forecast_values
+
+    return forecasts
 
 
 @government.route('/build_arima')
 def build_arima():
-    dataset = buildARIMADataset()
-    forecasts = forecast_weight(dataset)
-    # Perform any additional processing or inference here
-    return jsonify({'dataset': dataset, 'forecasts': forecasts})
+    weightDataset = buildWeightDataset()
+    weightForecasts = forecastWeight(weightDataset)
+    weightForecasts = {category: forecast for category, forecast in weightForecasts.items()}
+    # print("Weight Forecasts:", weightForecasts)
+
+    timeDataset = buildTimeDataset()
+    timeForecasts = forecastTime(timeDataset)
+    timeForecasts = {category: forecast for category, forecast in timeForecasts.items()}
+    print("Time Forecasts:", timeForecasts)
+    return jsonify({'dataset': timeDataset, 'forecasts': weightForecasts})
